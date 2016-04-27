f73426734512b05d22c746bdd45603b30dfc19d2
Daniel Schäfer <schaefer.dev@gmail.com>
2016-04-27 13:29:31 +0200

    rework of ex 1

--- sheet01/exercise01/filter-Q1/gauss.m
+++ sheet01/exercise01/filter-Q1/gauss.m
@@ -1,8 +1,15 @@
-function [G,x] = gauss(sigma)
+function [G] = gauss(sigma) % we deleted output x because it didn't make sense in our opinion, and it's also not meantioned on the sheet
+
+    % width and height of the filter
     dim = 6*sigma+1;
+
+    % Distance of pixel to filteredge
     dim2= 3 * sigma;
-    % We have a 2D Gaussian so the indices are in 2D
+
+    % local coordinates with respect to the center of gauss kernel
     x = zeros(dim,dim,2);
+
+    % Result
     G = zeros(dim,dim);
     
     for i=1:dim
@@ -12,6 +19,5 @@ function [G,x] = gauss(sigma)
         G(i,j) = 1.0 / (sqrt(2 * pi) * sigma) * exp(-(x(i,j,1)*x(i,j,1) + x(i,j,2)*x(i,j,2)) / (2 * sigma * sigma));
        end 
     end
-    
     
 end
