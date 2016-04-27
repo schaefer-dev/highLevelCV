d1b7c543d6089469d90d028c1de49f42ba661dfc
Daniel Schäfer <schaefer.dev@gmail.com>
2016-04-27 14:06:17 +0200

    1b maybe?

---
+++ sheet01/exercise01/filter-Q1/gauss1b.m
@@ -0,0 +1,21 @@
+% Faster version of gauss, because we use the fact that 2D Gaussian filter is seperable
+function [G] = gauss1b(sigma) % we deleted output x because it didn't make sense in our opinion, and it's also not meantioned on the sheet
+
+    % width and height of the filter
+    dim = 6*sigma+1;
+
+    % Distance of pixel to filteredge
+    dim2= 3 * sigma;
+
+    % local coordinates with respect to the center of gauss kernel
+    x = zeros(dim,1);
+
+    % Result
+    G = zeros(dim,1);
+    
+    for i=1:dim
+        x(i) = i-dim2-1;
+        G(i) = 1.0 / (sqrt(2 * pi) * sigma) * exp(-(x(i)) / (2 * sigma * sigma));
+    end
+    
+end
--- sheet01/exercise01/filter-Q1/gaussianfilter.m
+++ sheet01/exercise01/filter-Q1/gaussianfilter.m
@@ -1,4 +1,13 @@
 function outimage=gaussianfilter(img,sigma)
- [G,x] = gauss(sigma);
- outimage= conv2(img,G,'same');
+    G = gauss1b(sigma);
+    width = (size(img,1));
+    height = (size(img,2));
+    for i=1:width
+       list = conv(img(i,:),G,'same');
+       outimage(i,:) = list;
+    end
+    for i=1:height
+        list = conv(outimage(:,i),G,'same');
+        outimage(:,i) = list;
+    end
 end
