183a597ca4201622b77db3e0ecbdfeba18ab0d30
Dominik Weber <weber.dominik93@web.de>
2016-04-28 12:55:31 +0200

    Question 2b should work

--- sheet01/exercise01/identification-Q234/normalized_hist.m
+++ sheet01/exercise01/identification-Q234/normalized_hist.m
@@ -6,10 +6,24 @@
 %  num_bins - number of bins in the histogram
 %
 function h = normalized_hist(img_gray, num_bins)
+img_gray=double(img_gray);
 
   assert(length(size(img_gray)) == 2, 'image dimension mismatch');
   assert(isfloat(img_gray), 'incorrect image type');
-
+  
+  data = zeros(1,num_bins);
+  for i=1:(size(img_gray,1))
+      for j=1:(size(img_gray,2))
+          index = uint8(img_gray(i,j)*num_bins/255);
+          data(index) = data(index)+1;
+      end
+  end
+  s = sum(data);
+  data = data ./ s; 
+  step=255/num_bins;
+  h = bar(1:step:255,data);
+  sum(data)
+end
   % compute the histogram of pixel intensity values
   % ...
 
