04e234da32871b653a1634551ca6c5422e6c1f5a
Dominik Weber <weber.dominik93@web.de>
2016-04-30 10:36:54 +0200

    gaussderiv someone pls check my work :D

--- sheet01/filter-Q1/gaussderiv.m
+++ sheet01/filter-Q1/gaussderiv.m
@@ -1,6 +1,10 @@
 function [imgDx,imgDy]=gaussderiv(img,sigma)
     G=gauss(sigma);
     D=gaussdx(sigma);
+    derivx= 1/3 .* [1 0 1;1 0 1;1 0 1];
+    derivy= 1/3 .* [1 1 1;0 0 0;1 1 1];
+    imgDx = conv2(conv2(img,D),derivx)
+    imgDy= conv2(conv2(img,D),derivx); 
 end
 
 % ...
