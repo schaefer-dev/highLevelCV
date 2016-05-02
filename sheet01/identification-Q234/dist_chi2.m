95f3808a792b01aaa35f5783c061b5487b3b25d9
Thomas Pohl <s9thpohl@stud.uni-saarland.de>
2016-05-02 12:40:05 +0200

    Corrected dist_chi2 st. division with 0 is not permitted and started to implment find_best_match.m

--- sheet01/identification-Q234/dist_chi2.m
+++ sheet01/identification-Q234/dist_chi2.m
@@ -26,19 +26,31 @@ function d = dist_chi2(x,y)
 
     if (dim_x == 1) 
        for i=1:bins_x(1)
-            error = error + (x(i) - y(i))^2 / (x(i) + y(i));
+            error = error + (x(i) - y(i))^2;
+            sum = (x(i) + y(i));
+            if (sum ~= 0)
+                error = error / sum; 
+            end
        end 
     elseif (dim_x == 2)
        for i=1:bins_x(1)
           for j=1:bins_x(2)
-              error = error + (x(i,j) - y(i,j))^2 / (x(i,j) + y(i,j));
+              error = error + (x(i,j) - y(i,j))^2;
+              sum = (x(i,j) + y(i,j));
+              if (sum ~= 0)
+                 error = error / sum; 
+              end
           end
        end
     elseif (dim_x == 3)
        for i=1:bins_x(1)
           for j=1:bins_x(2)
               for k=1:bins_x(2)
-                 error = error + (x(i,j,k) - y(i,j,k))^2 / (x(i,j,k) + y(i,j,k));
+                 error = error + (x(i,j,k) - y(i,j,k))^2;
+                 sum = (x(i,j,k) + y(i,j,k));
+                 if (sum ~= 0)
+                    error = error / sum; 
+                 end
               end
           end
        end  
