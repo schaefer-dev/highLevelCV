45d13ad85e86e347fb608d3320b317f2d4bb0d28
Dominik Weber <weber.dominik93@web.de>
2016-05-09 17:16:59 +0200

    q2 done

--- sheet02/harris.m
+++ sheet02/harris.m
@@ -5,5 +5,21 @@
 
 
 function [px py M] = harris(img, sigma, thresh)
-  
-  % ... 
+
+alpha = 0.05;
+sig2 = 1.5 * sigma;
+G = gauss(sig2);
+
+[imgDx,imgDy]=gaussderiv(img,sigma);
+
+GDxx = sigma^2 .* gaussianfilter(imgDx .^ 2,sig2);
+GDyy = sigma^2 .* gaussianfilter(imgDy .^ 2,sig2);
+GDxy = sigma^2 .* gaussianfilter(imgDx .* imgDy,sig2);
+
+Cdet = (GDxx .* GDyy - GDxy .^ 2);
+Ctrace = (GDxx + GDyy);
+
+M = Cdet - alpha .* Ctrace .^ 2;
+imgMax = nonmaxsup2d(M);
+[py,px] = find(imgMax > thresh);
+end
