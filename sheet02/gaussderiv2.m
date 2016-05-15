fde65c20be30256d0418811a81585a49be31c019
Daniel Schäfer <schaefer.dev@gmail.com>
2016-05-15 14:01:16 +0200

    cleanup comments

--- sheet02/gaussderiv2.m
+++ sheet02/gaussderiv2.m
@@ -12,26 +12,7 @@ function [imgDxx, imgDxy, imgDyy] = gaussderiv2(img,sigma)
   [imgDx,  imgDy ] = gaussderiv(img,   sigma);
   [imgDxx, imgDxy] = gaussderiv(imgDx, sigma);
   [~,      imgDyy] = gaussderiv(imgDy, sigma);
-  
-  %{
-  G = gauss(sigma);
-  Dx = gaussdx(sigma);
-  Dxx = gaussdxx(sigma);
-  
-  imgDxx = conv2(img, Dxx, 'same');
-  imgDxx = conv2(imgDxx, G', 'same');
-  imgDxx = conv2(imgDxx, G', 'same');
 
-  imgDxy = conv2(img, Dx, 'same');
-  imgDxy = conv2(imgDxy, Dx', 'same');
-  imgDxy = conv2(imgDxy, G, 'same');
-  imgDxy = conv2(imgDxy, G', 'same');
-
-  imgDyy = conv2(img, G, 'same');
-  imgDyy = conv2(imgDyy, Dxx', 'same');
-  imgDyy = conv2(imgDyy, G, 'same');
-  imgDyy = conv2(imgDyy, G', 'same');
-  %}
-  % ... 
+  
 end
 
