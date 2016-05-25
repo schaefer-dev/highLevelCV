d418c0191e4c4d1ac77874c76a24e3bc218756fe
Daniel Schäfer <schaefer.dev@gmail.com>
2016-05-26 00:34:20 +0200

    typo fixed

--- sheet03/matlab/show_false_detections.m
+++ sheet03/matlab/show_false_detections.m
@@ -49,9 +49,9 @@ function show_false_detections(figidx, pos_test_list, pos_class_score, neg_test_
     for i=1:num_show
         worst = 0.0;
         worstindex = 0;
-        for i=1:length(pos_class_score)
-            if pos_class_score(i) > worst
-                worst = pos_class_score(i);
+        for i=1:length(neg_class_score)
+            if neg_class_score(i) > worst
+                worst = neg_class_score(i);
                 worstindex = i;
             end
         end
@@ -62,7 +62,7 @@ function show_false_detections(figidx, pos_test_list, pos_class_score, neg_test_
         else
             % set the worst value found to zero to avoid finding it
             % twice
-            pos_class_score(worstindex) = 0.0;
+            neg_class_score(worstindex) = 0.0;
             % TODO we have to some stuff now with the picture we identified
             % as the worst (at index worstindex) in the current state, this 
             % is being reached num_show times
