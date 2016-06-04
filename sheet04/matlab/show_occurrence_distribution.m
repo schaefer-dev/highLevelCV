397ab2f3313601a567c44e74265dac4eb44c63ed
Daniel Schäfer <schaefer.dev@gmail.com>
2016-06-04 02:14:37 +0200

    comments to clarify stuff

--- sheet04/matlab/show_occurrence_distribution.m
+++ sheet04/matlab/show_occurrence_distribution.m
@@ -1,22 +1,23 @@
 function show_occurrence_distribution(cluster_occurrences, cluster_idx)
 
   % ...
-  % hardcoding the ranges
   figure(7)
   Xplot = [];
   Yplot = [];
   
- 
+  % collecting all the data to be plotted afterwards
   for i=1:size(cluster_occurrences,1)
       if cluster_occurrences(i,1)==cluster_idx
           Xplot = [Xplot cluster_occurrences(i,2)];
           Yplot = [Yplot cluster_occurrences(i,3)];
       end
   end
-  %...
   
-  
+  % plots data and a star in the center
   plot(Xplot,Yplot,'.',0,0,'*');
+  
+  % hardcoded limitation for 300x100 pictures
   xlim([-150,150]);
   ylim([-50,50]);
+  %...
 end
