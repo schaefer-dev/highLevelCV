8a7095d0d6457b9c4b688df5e8f73d89ee4f18f6
Daniel Schäfer <schaefer.dev@gmail.com>
2016-06-04 02:11:15 +0200

    Ex2 finished show_occurence_distribution implemented and tested

--- sheet04/matlab/show_occurrence_distribution.m
+++ sheet04/matlab/show_occurrence_distribution.m
@@ -1,3 +1,22 @@
 function show_occurrence_distribution(cluster_occurrences, cluster_idx)
 
   % ...
+  % hardcoding the ranges
+  figure(7)
+  Xplot = [];
+  Yplot = [];
+  
+ 
+  for i=1:size(cluster_occurrences,1)
+      if cluster_occurrences(i,1)==cluster_idx
+          Xplot = [Xplot cluster_occurrences(i,2)];
+          Yplot = [Yplot cluster_occurrences(i,3)];
+      end
+  end
+  %...
+  
+  
+  plot(Xplot,Yplot,'.',0,0,'*');
+  xlim([-150,150]);
+  ylim([-50,50]);
+end
