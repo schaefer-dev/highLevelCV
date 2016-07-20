Fixed input normalization for handpose

Project/Code/handpose_estimator.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn import preprocessing
 from features import getHOG
 from features import getLBP
+from sklearn.preprocessing import StandardScaler
 '''
 It is a bit unclear what this function should do yet. We have several options:
 
@@ -66,23 +67,29 @@ def prepare_images(images, scale=0.5):
             X.append(lbp)
 
     X = np.asarray(X)
-    X = preprocessing.scale(X.astype(np.double))
     return X
 
 def handpose_estimator(images, Y, scale = 0.5):
     print "Train Handpose Estimator..."
     #Y = convert_classes(np.asarray(Y))
     Y = np.asarray(Y)
-    #scale = 0.5
 
     # prepare training data
     X = prepare_images(images, scale)
+    scaler = StandardScaler(copy=False)
+    X = scaler.fit_transform(X)
 
     # Train classifier
-    #clf = SVC(C=0.1)
-    clf = RandomForestClassifier(n_estimators=10, n_jobs=3, class_weight="balanced")
+    clf = LinearSVC()
+    #clf = RandomForestClassifier(n_estimators=10, n_jobs=3, class_weight="balanced")
     clf.fit(X, Y)
 
+    return (scaler,clf)
 
+def run_handpose_estimator((scaler, clf), images, scale = 0.5):
+    # prepare training data
+    X = prepare_images(images, scale)
+    X = scaler.transform(X)
 
-    return clf
+    pred = clf.predict(X)
+    return pred

Project/Code/main.py
@@ -7,6 +7,7 @@ import os
 from load_data import load_training_data
 from headpose_estimator import headpose_estimator
 import handpose_estimator
+from handpose_estimator import run_handpose_estimator
 from get_bow import get_bow
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
@@ -22,7 +23,7 @@ def main():
     data_location = os.path.join(os.path.dirname(__file__), '../data/')
 
     # Get Training and Test Data
-    training_pcipants = 10
+    training_pcipants = 4
     (images, Y, image_names) = load_training_data(data_location, num_participants=training_pcipants, scale=scale)
 
     if headClassifier:
@@ -34,7 +35,7 @@ def main():
     # Get Handposes/Handpositions/Classifications based on Hands, whatever we want to do here
     hand_clf = None
     if handClassifier:
-        hand_clf = handpose_estimator.handpose_estimator(images, Y, scale=scale)
+        (scaler, hand_clf) = handpose_estimator.handpose_estimator(images, Y, scale=scale)
 
     ################## TESTING ########################
 
@@ -44,11 +45,11 @@ def main():
 
     # Perform validation
     if handClassifier:
-        (images, Y, image_names) = load_training_data(data_location, num_participants=4, scale=scale, skip=training_pcipants)
+        (images, Y, image_names) = load_training_data(data_location, num_participants=1, scale=scale, skip=training_pcipants)
         #Y = handpose_estimator.convert_classes(np.asarray(Y))
         Y = np.asarray(Y)
-        X = handpose_estimator.prepare_images(images, scale)
-        pred = hand_clf.predict(X)
+
+        pred = run_handpose_estimator((scaler, hand_clf), images, scale=scale)
         print classification_report(Y, pred)
         cm = confusion_matrix(Y, pred)
         plot_confusion_matrix(cm)